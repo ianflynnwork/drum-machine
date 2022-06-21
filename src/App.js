bcb467a64dac2303c4854f2dfbcf9c1b968f4150
ianflynnwork <ianflynnwork@gmail.com>
2022-06-21 15:29:14 -0400

    drumpad audio functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from "react";
-import DrumPads from "./comps/DrumPads";
+import React, { useState } from "react";
+import DrumPad from "./comps/DrumPad";
 
 
 function App() {
+  const [soundText, setSoundText] = useState('');
+  
+  const drumData = [
+    {keyCode: 81, id: 'snare1', letter: 'Q', src: 'https://drumsoundz.com/Snare1.wav'},
+    {keyCode: 87, id: 'snare2', letter: 'W', src: 'https://drumsoundz.com/Snare2.wav'},
+    {keyCode: 69, id: 'snare3', letter: 'E', src: 'https://drumsoundz.com/Snare3.wav'},
+    {keyCode: 65, id: 'closed-hat', letter: 'A', src: 'https://drumsoundz.com/Hat%20Closed.wav'},
+    {keyCode: 83, id: 'symbol1', letter: 'S', src: 'https://drumsoundz.com/Crash-01.wav'},
+    {keyCode: 68, id: 'symbol2', letter: 'D', src: 'https://drumsoundz.com/Crash-02.wav'},
+    {keyCode: 90, id: 'symbol3', letter: 'Z', src: 'https://drumsoundz.com/Crash-03.wav'},
+    {keyCode: 88, id: 'bass1', letter: 'X', src: 'https://drumsoundz.com/Bassdrum-02.wav'},
+    {keyCode: 67, id: 'bass2', letter: 'C', src: 'https://drumsoundz.com/Bassdrum-03.wav'},
+    //{id: 'bass3', letter: 'V', src: 'https://drumsoundz.com/Bassdrum-04.wav'},
+    //{id: 'sfx1', letter: 'F', src: 'https://drumsoundz.com/SFX-03.wav'},
+    //{id: 'sfx2', letter: 'R', src: 'https://drumsoundz.com/SFX-04.wav'},
+    //{id: 'sfx3', letter: 'T', src: 'https://drumsoundz.com/Tom%20H.wav'},
+    //{id: 'sfx4', letter: 'G', src: 'https://drumsoundz.com/Tom%20L.wav'}
+  ];
+
   return (
     <div id='drum-machine'>
       <div id="display"></div>
-      <DrumPads />
+      {drumData.map(pad => (
+        <DrumPad 
+          pad={pad}
+          key={pad.id}
+          setSoundText={ setSoundText }
+        />
+      ))}
+      
     </div>
   );
 }
---
+++ src/comps/DrumPad.js
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+
+
+
+function DrumPad({ pad }) {
+    
+    useEffect(() => {
+        document.addEventListener('keydown', handleKeyPress);
+        return () => {
+            document.removeEventListener('keydown', handleKeyPress)
+        };
+    }, [])
+
+    const handleKeyPress = (e) => {
+        if(e.keyCode === pad.keyCode){
+            playSound();
+         
+        }
+    }
+    const playSound = () => {
+        const audioTag = document.getElementById(pad.letter);
+        audioTag.play();
+    }
+    
+
+    return (
+        <div 
+        className="drum-pad" 
+        id={pad.id}
+        onClick={playSound}>
+            <p>{pad.letter}</p>
+            <audio 
+                src={pad.src} 
+                className="clip" 
+                id={pad.letter}>
+            </audio>
+        </div>
+    )
+}
+
+export default DrumPad;
